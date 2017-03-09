132e734852e989675f3d512530c3399d366e95f8
Klim Agarkov <jewelgatorstv@gmail.com>
2017-03-09 20:24:41 +0300

    Add routes table.

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,48 +1,48 @@
 class TrainsController < ApplicationController
-before_action :set_train, only: [:show, :edit, :update, :destroy]
+  before_action :set_train, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@trains = Train.all
-	end
+  def index
+    @trains = Train.all
+  end
 
-	def show
-		#@train = Train.find(params[:id])
-	end
+  def show
+    @train = Train.find(params[:id])
+  end
 
-	def new
-		@train = Train.new
-	end
+  def new
+    @train = Train.new
+  end
 
-	def create
-		@train = Train.new(train_params)
+  def create
+    @train = Train.new(train_params)
 
-		if @train.save
-			redirect_to @train
-		else
-			render :new
-		end
-	end
+    if @train.save
+      redirect_to @train
+    else
+      render :new
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @train.update(train_params)
-			redirect_to @train
-		else
-			render :edit
-		end
-	end
+  def update
+    if @train.update(train_params)
+      redirect_to @train
+    else
+      render :edit
+    end
+  end
 
-	def destroy
-		@train.destroy
-		redirect_to trains_path, notice: "Поезд удален"
-	end
-
-
+  def destroy
+    @train.destroy
+    redirect_to trains_path, notice: "Поезд удален"
+  end
 
 
-	 private
+
+
+   private
     
     def set_train
       @train = Train.find(params[:id])
